fe9e0edf733f59479e0ed614e14c3e902ffc2527
Kumari <kumarivenkata994@gmail.com>
2026-02-10 20:44:29 +0530

    code

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "security_group_ids" {
+    default = ["sg-07a79621c14f8682e"]
+}
+
+variable "tags" {
+    default = {
+        Name = "roboshop-cart"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
